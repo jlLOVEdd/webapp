9112874a6aa964a4c9b5d9946139f4c4362e3c1f
dongdong.wei <dongdong.wei@ikang.com>
2018-02-05 15:50:05 +0800

    mybatis dao 接口方式  下一步 抽离baseDao

--- src/main/java/com/wdd/test/service/BaseService.java
+++ src/main/java/com/wdd/test/service/BaseService.java
@@ -9,54 +9,45 @@ import com.wdd.test.bean.PageParameter;
 import com.wdd.test.dao.BaseDao;
 
 
-
 @Service
 @Transactional(readOnly = true)
-public abstract class BaseService<T>
-{
-	protected Logger	logger	= LoggerFactory.getLogger(getClass());
+public abstract class BaseService<T> {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
-	public abstract BaseDao<T> getDao();
+    public abstract BaseDao<T> getDao();
 
-	public PageParameter queryForPage(PageParameter page, T t)
-	{
-		return getDao().queryForPage(page, t);
-	}
+    public PageParameter queryForPage(PageParameter page, T t) {
+        return getDao().queryForPage(page, t);
+    }
 
-	@Transactional(readOnly = false)
-	public void save(T t)
-	{
-		this.getDao().save(t);
-	}
+    @Transactional(readOnly = false)
+    public void save(T t) {
+        this.getDao().save(t);
+    }
 
-	@Transactional(readOnly = false)
-	public String[] saveOrder(T t)
-	{
-		this.getDao().save(t);
-		return null;
-	}
-	
-	@Transactional(readOnly = false)
-	public void update(T t)
-	{
-		this.getDao().update(t);
-	}
+    @Transactional(readOnly = false)
+    public String[] saveOrder(T t) {
+        this.getDao().save(t);
+        return null;
+    }
 
-	@Transactional(readOnly = false)
-	public void delete(Long... ids)
-	{
-		this.getDao().delete(ids);
-	}
+    @Transactional(readOnly = false)
+    public void update(T t) {
+        this.getDao().update(t);
+    }
 
-	@Transactional(readOnly = true)
-	public T getObjectById(Long id)
-	{
-		return this.getDao().selectById(id);
-	}
+    @Transactional(readOnly = false)
+    public void delete(Long... ids) {
+        this.getDao().delete(ids);
+    }
 
-	@Transactional(readOnly = false)
-	public void executeSql(String sql)
-	{
-		this.getDao().getJdbcTemplate().execute(sql);
-	}
+    @Transactional(readOnly = true)
+    public T getObjectById(Long id) {
+        return this.getDao().selectById(id);
+    }
+
+    @Transactional(readOnly = false)
+    public void executeSql(String sql) {
+        this.getDao().getJdbcTemplate().execute(sql);
+    }
 }
