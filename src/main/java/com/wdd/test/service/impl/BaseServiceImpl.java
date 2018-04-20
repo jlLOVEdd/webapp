8c121e761f94b6de8b2603f183572c17fdae5871
dongdong.wei <dongdong.wei@ikang.com>
2018-04-20 10:06:42 +0800

    构建<groal>添加

--- src/main/java/com/wdd/test/service/impl/BaseServiceImpl.java
+++ src/main/java/com/wdd/test/service/impl/BaseServiceImpl.java
@@ -1,18 +1,14 @@
 package com.wdd.test.service.impl;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
-
 import com.wdd.test.dao.BaseMapper;
 import com.wdd.test.service.BaseService;
-import sun.plugin2.message.Serializer;
 
-import javax.annotation.Resource;
 import java.io.Serializable;
 
 
-public  abstract class BaseServiceImpl<T,ID extends Serializable> implements BaseService<T,ID> {
+public abstract class BaseServiceImpl<T, ID extends Serializable> implements BaseService<T, ID> {
 
-     abstract  BaseMapper<T,ID> getBaseMapper();
+    abstract BaseMapper<T, ID> getBaseMapper();
 
     @Override
     public int insert(T entity) {
