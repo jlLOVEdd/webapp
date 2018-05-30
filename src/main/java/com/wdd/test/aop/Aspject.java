e6ab2658a616d1c5561cbc05ab483055277fdc92
wdd <18515140641@163.com>
2018-05-30 22:52:10 +0800

    给媳妇儿的作业

--- src/main/java/com/wdd/test/aop/Aspject.java
+++ src/main/java/com/wdd/test/aop/Aspject.java
@@ -8,20 +8,23 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class Aspject {
-	public Aspject() {
-		// TODO Auto-generated constructor stub
-	}
-	@Pointcut("execution(* *add*(..))")
-	public void addmethod(){
-		System.out.println("I am pointcut method");
-	}
-	@Before("addmethod()")
-	public void setLOg(){
-		System.out.println("currentTime:"+System.currentTimeMillis()+"----before");
-	}
-	@After("addmethod()")
-	public void setAfterLOg(){
-		System.out.println("currentTime:"+System.currentTimeMillis()+"----After");
-	}
-	
+    public Aspject() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Pointcut("execution(* *add*(..))")
+    public void addmethod() {
+        System.out.println("I am pointcut method");
+    }
+
+    @Before("addmethod()")
+    public void setLOg() {
+        System.out.println("currentTime:" + System.currentTimeMillis() + "----before");
+    }
+
+    @After("addmethod()")
+    public void setAfterLOg() {
+        System.out.println("currentTime:" + System.currentTimeMillis() + "----After");
+    }
+
 }
